1629032f6c0372d5b511b2eba82b9f84f99a2808
coma-dwc <tamagomapon@gmail.com>
2023-08-22 22:11:29 +0900

    ファイルの存在確認

--- src/file/Main.java
+++ src/file/Main.java
@@ -1,11 +1,35 @@
 package file;
 
+//Fileクラスを使用するために下記をimport
+//import java.io.File;
+//
+//public class Main {
+//	public static void main(String[] args) {
+////		File file = new File("abc.txt");
+//		File dir = new File("dir1");
+//		File file = new File(dir, "abc.txt");
+////		File file = new File("dir1/abc.txt");
+//		System.out.println(file);
+//		System.out.println(file.getName());
+//	}
+//}
+
+
+//ファイルの存在確認
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		File file = new File("abc.txt");
-		System.out.println(file);
-		System.out.println(file.getName());
+		File file1 = new File("abc.txt"); //存在しない
+		File file2 = new File("Main.java"); //存在する
+		
+		for(File file : Arrays.asList(file1, file2)) {
+			System.out.println("File = " + file);
+			System.out.println("exists = " + file.exists());  //exists()返り値boolean
+			System.out.println("isFile = " + file.isFile()); //isFile()返り値boolean
+			System.out.println("isDirectory = " + file.isDirectory());  //isDirectory()返り値boolean
+			System.out.println("length = " + file.length());  //length()返り値long
+		}
 	}
 }
