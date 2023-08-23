e4a278f0be3406f553759b522d23a75ed2369b7d
coma-dwc <tamagomapon@gmail.com>
2023-08-23 15:49:06 +0900

    Files#writeを使う

--- src/binary_make/Main.java
+++ src/binary_make/Main.java
@@ -1,11 +1,16 @@
 package binary_make;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.List;
+
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.util.List;
 
 ///FileOutputStreamを使う
 //FileOutputStreamを使ってバイナリデータをFileに保存するwriteBinaryToFileを完成させる
@@ -43,6 +48,44 @@ import java.util.List;
 //writeBinaryToFileをデータの追加書き込みが出来るように改良
 //第3引数のappendがtrueの場合追加書き込み / falseの場合、既存のファイルがあったとしても無視して新規でファイルを作成
 
+//public class Main {
+//	private static List<String> readTextLines(File file) throws IOException {
+//		return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+//	}
+//	
+//	private static void handleLine(String line) {
+//		System.out.println(line);
+//	}
+//	
+//	private static void writeBinaryToFile(File file, byte[] data, boolean append) throws IOException {
+//		try(FileOutputStream fos = new FileOutputStream(file, append)) {
+//			fos.write(data);
+//		}
+//	}
+//	
+//	public static void main(String[] args) throws IOException {
+//		File file = new File("4-2.txt");
+//		String text = "この文字列をファイルに書き込みます。 \n2行目です。 \n";
+//		writeBinaryToFile(file, text.getBytes("UTF-8"), false);
+//		
+//		//再度同じファイルに追加書き込み
+//		writeBinaryToFile(file, text.getBytes("UTF-8"), true);
+//		
+//		//書き込んだデータをコンソールに出力
+//		for(String line : readTextLines(file)) {
+//			handleLine(line);
+//		}
+//	}
+//}
+
+
+///Files#writeを使う
+//writeBinaryToFileをFiles#writeを使って書き換え
+
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class Main {
 	private static List<String> readTextLines(File file) throws IOException {
 		return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
@@ -53,10 +96,11 @@ public class Main {
 	}
 	
 	private static void writeBinaryToFile(File file, byte[] data, boolean append) throws IOException {
-		try(FileOutputStream fos = new FileOutputStream(file, append)) {
-			fos.write(data);
-		}
+		OpenOption[] options = append ? new OpenOption[] { StandardOpenOption.APPEND } : new OpenOption[] {};  //追加書き込みの場合はOpenOptionとしてStandardOptionOption.APPENDを指定 /そうでない場合はデフォルトのままでいいので空の配列を作成
+		Files.write(file.toPath(), data, options);  //Filesのwriteメソッドを使ってデータを書き込む 引数はPathなのでtoPath()メソッドを使用してFileをPathに変換する
 	}
+	//↑ここでは配列を使用していますが、writeメソッドでのOpenOptionは可変引数なので次のように呼び出すことも出来る=> Files.write(file.toPath(), data, StandardOpenOption.APPEND);
+	
 	
 	public static void main(String[] args) throws IOException {
 		File file = new File("4-2.txt");
